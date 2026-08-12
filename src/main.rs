f7191840b26b72e4fa6860220518712fa3fcdba1
ansh3108 <anshkumar9686@gmail.com>
2026-08-12 16:32:04 +0530

    add winit and implement window event loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use std::process::exit;
+
 mod chunk;
 mod mesh;
 
 fn main() {
-    let mut my_chunk = chunk::Chunk::new();
-    my_chunk.set_block(5, 5, 5, 1);
+    let event_loop = winit::event_loop::EventLoop::new().unwrap(); 
+    
+    let window = winit::window::WindowBuilder::new()
+    .with_title("Voxel Engine")
+    .build(&event_loop)
+    .unwrap();
 
-    mesh::generate_mesh(&my_chunk);
+    event_loop.run(move |event, elwt| {
+        match event {
+            winit::event::Event::WindowEvent { 
+                event: winit::event::WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("Closing window!");
+                elwt.exit(); 
+            }
+            _ => ()
+        }
+    }).unwrap();
 }
