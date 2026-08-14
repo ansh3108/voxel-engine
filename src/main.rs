6b848955ba016527034fdbe21b4bc9f3ea0a0294
ansh3108 <anshkumar9686@gmail.com>
2026-08-14 17:45:38 +0530

    add render pipeline setup and vertex shader implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use std::process::exit;
-
-use winit::{event, window};
-
 mod chunk;
 mod mesh;
 
@@ -10,7 +6,8 @@ struct State {
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
     impl State {
@@ -40,6 +37,44 @@ struct State {
             
             let config = surface.get_default_config(&adapter, size.width, size.height).unwrap();
 
+            let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+            let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor{
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState { module: &shader, entry_point: "vs_main", buffers: &[] },
+            
+            
+            
+                fragment: Some(wgpu::FragmentState{
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState { format: config.format, blend: Some(wgpu::BlendState::REPLACE), write_mask: wgpu::ColorWrites::ALL, })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+                });
+
             surface.configure(&device, &config);
 
             Self {
@@ -47,7 +82,8 @@ struct State {
                 device,
                 queue,
                 config,
-                size
+                size,
+                render_pipeline,
             }
         }
 
@@ -60,7 +96,7 @@ struct State {
             });
 
             {
-                let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                     label: Some("Render Pass"),
                     color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                         view: &view,
@@ -74,6 +110,9 @@ struct State {
                     timestamp_writes: None,
                     occlusion_query_set: None,
                 });
+
+                render_pass.set_pipeline(&self.render_pipeline);
+                render_pass.draw(0..3, 0..1);
             }
 
             self.queue.submit(std::iter::once(encoder.finish()));
